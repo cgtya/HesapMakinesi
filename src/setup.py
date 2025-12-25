2abe5537e4e60d6ecf3930f759702c2e780c9fd7
cgtya <cagataysln06@outlook.com>
2025-12-25 13:22:28 +0300

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,22 +1,30 @@
+import os
 import sys
-from cx_Freeze import setup, Executable
+
+from cx_Freeze import Executable, setup
+
 sys.setrecursionlimit(5000)
 
 
 build_options = {
-    'packages': ['torch','torchvision'], 
-    'excludes': [],
-    'include_files': [('ocr_src', 'lib/ocr_src')],
+    "packages": ["torch", "torchvision"],
+    "excludes": [],
+    "include_files": [("ocr_src", os.path.join("lib", "ocr_src"))],
+    "optimize": 2,
 }
 
-base = 'gui'
+base = "gui"
 
 executables = [
-    Executable('src/gui_test.py', base=base, target_name="HesapMakinesi")
+    Executable(
+        os.path.join("src", "gui_test.py"), base=base, target_name="HesapMakinesi"
+    )
 ]
 
-setup(name='HesapMakinesi',
-      version = '1.0',
-      description = 'Verilen görseldeki matematiksel ifadeyi tanımlayıp adım adım çözümünü sunabilen hesap makinesi uygulaması.',
-      options = {'build_exe': build_options},
-      executables = executables)
+setup(
+    name="HesapMakinesi",
+    version="1.0",
+    description="Verilen görseldeki matematiksel ifadeyi tanımlayıp adım adım çözümünü sunabilen hesap makinesi uygulaması.",
+    options={"build_exe": build_options},
+    executables=executables,
+)
